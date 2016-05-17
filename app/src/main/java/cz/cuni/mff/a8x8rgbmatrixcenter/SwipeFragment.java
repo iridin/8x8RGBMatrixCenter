3d134fcb7b6edac04d3bca809127e2fd9a831204
Dominik Skoda <skoda@d3s.mff.cuni.cz>
2016-05-17 21:03:44 +0200

    Handling onTouch events in the SwipeFragment.

--- app/src/main/java/cz/cuni/mff/a8x8rgbmatrixcenter/SwipeFragment.java
+++ app/src/main/java/cz/cuni/mff/a8x8rgbmatrixcenter/SwipeFragment.java
@@ -1,11 +1,14 @@
 package cz.cuni.mff.a8x8rgbmatrixcenter;
 
 import android.app.Fragment;
+import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -21,6 +24,44 @@ public class SwipeFragment extends Fragment {
     private MatrixActivity mActivity;
     private ColorChainAdapter mColorChainAdapter;
     private LEDView[] leds = new LEDView[LED_ARRAY_HEIGHT * LED_ARRAY_WIDTH];
+
+
+
+    private class LedOnTouchListener implements View.OnTouchListener {
+
+        private SwipeFragment mParent;
+
+        public LedOnTouchListener(SwipeFragment parent){
+            mParent = parent;
+        }
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_UP){
+                for(int i = 0; i < LED_ARRAY_HEIGHT * LED_ARRAY_WIDTH; i++) {
+                    LEDView view = mParent.leds[i];
+                    if (view.getColor() != Color.BLACK){
+                        view.setColor(Color.BLACK);
+                        view.invalidate();
+                    }
+                }
+                return true;
+            }
+
+            for(int i = 0; i < LED_ARRAY_HEIGHT * LED_ARRAY_WIDTH; i++) {
+                LEDView view = mParent.leds[i];
+                Rect outRect = new Rect(view.getLeft(), view.getTop(), view.getRight(), view.getBottom());
+                if(outRect.contains((int)event.getX(), (int)event.getY())) {
+                    view.setColor(Color.RED);
+                    view.invalidate();
+                } else if (view.getColor() != Color.BLACK){
+                    view.setColor(Color.BLACK);
+                    view.invalidate();
+                }
+            }
+            return true;
+        }
+    }
 
     public SwipeFragment() {
         // Empty constructor required for fragment subclasses
@@ -65,16 +106,17 @@ public class SwipeFragment extends Fragment {
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
         itemTouchHelper.attachToRecyclerView(colorChain);
 
+
         // Initialize LED matrix
         MatrixView matrixView = (MatrixView) rootView.findViewById(R.id.matrixView);
         matrixView.setColorSelection(null);
+        matrixView.setOnTouchListener(new LedOnTouchListener(this));
         for(int i = 0; i < LED_ARRAY_HEIGHT * LED_ARRAY_WIDTH; i++) {
             View view = inflater.inflate(R.layout.led_layout, matrixView, false);
             LEDView ledView = (LEDView) view.findViewById(R.id.led_view);
 
             ledView.setLedMatrix(matrixView);
             matrixView.addView(ledView);
-            ledView.setOnClickListener(matrixView);
 
             leds[i] = ledView;
         }
